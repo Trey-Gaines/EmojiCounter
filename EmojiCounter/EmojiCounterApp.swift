78f832eaf49ac9102219b99f6dcc26df9ad5064c
Trey G <dove-ascent-0q@icloud.com>
2025-07-01 14:47:52 -0400

    Initial Commit

---
+++ EmojiCounter/EmojiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterApp.swift
+//  EmojiCounter
+//
+//  Created by Trey Gaines on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiCounterTests/EmojiCounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiCounterTests.swift
+//  EmojiCounterTests
+//
+//  Created by Trey Gaines on 7/1/25.
+//
+
+import Testing
+
+struct EmojiCounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
